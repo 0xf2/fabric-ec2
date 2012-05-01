aaa877d2b92f89474eb9e406cd80959d79f660cb
Mike Ryan <mike@fadedink.co.uk>
2012-05-01 11:08:19 +0200

    setup files work better when they have some contents

--- setup.py
+++ setup.py
@@ -1,1 +1,12 @@
+#!/usr/bin/env python
 
+from distutils.core import setup
+
+setup(name='fabric-ec2',
+      version='0.1',
+      author='Mike Ryan',
+      author_email='mike@fadedink.co.uk',
+      url='https://github.com/mikery/fabric-ec2',
+      packages=['fabric_ec2', ],
+      requires=['boto', 'fabric'],
+      )
