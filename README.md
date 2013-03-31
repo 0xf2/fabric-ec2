2d0a6266c5f3d8f954b2548fd240557edb26b961
Mike Ryan <mike@fadedink.co.uk>
2013-03-31 18:13:22 +0200

    updated to class-based helper

---
+++ README.md
@@ -0,0 +1,39 @@
+Some helpers to make running Fabric tasks on EC2 instances a little easier.
+
+You can use [EC2 Tags](http://docs.aws.amazon.com/AWSCloudFormation/latest/UserGuide/aws-properties-ec2-tags.html) to assign tags to your instances describing their roles. For example, your webserver instances could be tagged with 'role=web'.
+
+This helper class lets you use these tags on conjunction with Fabric's role definitions, by dynamically querying the EC2 API to find the instances that have been assigned a particular role.
+
+Usage::
+    from fabric.api import run, sudo, env
+    from fabric_ec2 import EC2TagManager
+
+    def configure_roles(environment):
+        """ Set up the Fabric env.roledefs, using the correct roles for the given environment
+        """
+        tags = EC2TagManager(AWS_KEY, AWS_SECRET,
+            regions=['eu-west-1'],
+            common_tags={'production': 'true'})
+
+        roles = {}
+        for role in ['web', 'db']:
+            roles[role] = tags.get_instances(role=role)
+
+        return roles
+
+    env.roledefs = configure_roles()
+
+    @roles('web')
+    def restart_web():
+        sudo('/etc/init.d/nginx restart')
+
+    @roles('db')
+    def restart_db():
+        sudo('/etc/init.d/postgresql restart')
+
+    def hostname():
+        run('hostname')
+
+    $ fab restart_db
+    $ fab restart_web
+    $ fab hostname --roles web
--- fabric_ec2/__init__.py
+++ fabric_ec2/__init__.py
@@ -1,32 +1,53 @@
 from boto import ec2
 
 
-def get_instances_for_tags(tags, ec2_regions=None, aws_key=None, aws_secret=None):
-    """ Returns a list of hosts, matching the given tags/ec2_regions.
-    """
-    # Use default regions (taken from boto) if user didn't specify a list
-    if not ec2_regions:
-        ec2_regions = ['us-east-1', 'us-west-1', 'us-west-2',
-                       'sa-east-1',
-                       'eu-west-1',
-                       'ap-southeast-1',
-                       'ap-northeast-1']
+class EC2TagManager:
+    def __init__(self, aws_access_key_id=None, aws_secret_access_key=None, regions=None, common_tags=None) :
+        """ This class helps find instances with a particular set of tags.
+        
+            If access key/secret are not given, they must be available as environment
+            variables so boto can access them.
+        """
+        self.aws_access_key_id = aws_access_key_id
+        self.aws_secret_access_key = aws_secret_access_key
+        self.common_tags = common_tags if common_tags else {}
+        # todo get full region list
+        self.regions = regions if regions else ['us-east-1']
+        # Open connections to ec2 regions
+        self.conn = {}
+        for region in self.regions:
+            self.conn[region] = ec2.connect_to_region(region,
+                aws_access_key_id=self.aws_access_key_id,
+                aws_secret_access_key=self.aws_secret_access_key)
 
-    tag_filter = {}
-    for key, val in tags.iteritems():
-        tag_filter['tag:%s' % key] = val
+    def _build_tag_filter(self, tags):
+        """ Convert a dict in to a tag filter.
+        
+            Given a dict {'key': 'val'}, return {'tag:key': 'val'}.
+        """
+        tag_filter = {}
+        for k, v in tags.iteritems():
+            tag_filter['tag:%s' % k] = v
+        return tag_filter
+            
 
-    host_list = []
-    for region in ec2_regions:
-        conn = ec2.connect_to_region(region,
-            aws_access_key_id=aws_key,
-            aws_secret_access_key=aws_secret)
-        reservations = conn.get_all_instances(None, tag_filter)
-        for res in reservations:
-            for instance in res.instances:
-                if instance.public_dns_name != '':
-                    # Terminated/stopped instances will not have a public_dns_name
-                    host_list.append(instance.public_dns_name)
-    return host_list
+    def get_instances(self, **kwargs):
+        """ Return instances that match the given tags. Tags are specified
+            as kwargs, e.g. "get_instances(role='test')
+        """
+        tags = self.common_tags
+        for k, v in kwargs.copy().iteritems():
+            tags[k] = v
+            kwargs.pop(k)
 
+        tag_filter = self._build_tag_filter(tags)
 
+        hosts = []
+        for region in self.regions:
+            reservations = self.conn[region].get_all_instances(None, tag_filter)
+            for res in reservations:
+                for instance in res.instances:
+                    if instance.public_dns_name != '':
+                        # Terminated/stopped instances will not have a public_dns_name
+                        hosts.append(instance.public_dns_name)
+        return hosts
