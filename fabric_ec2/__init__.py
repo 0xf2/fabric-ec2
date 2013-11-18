26635ee35ec38edc029dee30a2d93478cfc0c0db
Lemur <lemur@Lemurs-MacBook-Pro.local>
2013-11-18 13:07:39 +0000

    Added ability to specify the instance attribute to return from get_instances as it allows retrieval of instances in private VPC subnets for example

--- fabric_ec2/__init__.py
+++ fabric_ec2/__init__.py
@@ -2,7 +2,7 @@ from boto import ec2
 
 
 class EC2TagManager:
-    def __init__(self, aws_access_key_id=None, aws_secret_access_key=None, regions=None, common_tags=None) :
+    def __init__(self, aws_access_key_id=None, aws_secret_access_key=None, regions=None, common_tags=None):
         """ This class helps find instances with a particular set of tags.
         
             If access key/secret are not given, they must be available as environment
@@ -17,8 +17,8 @@ class EC2TagManager:
         self.conn = {}
         for region in self.regions:
             self.conn[region] = ec2.connect_to_region(region,
-                aws_access_key_id=self.aws_access_key_id,
-                aws_secret_access_key=self.aws_secret_access_key)
+                                                      aws_access_key_id=self.aws_access_key_id,
+                                                      aws_secret_access_key=self.aws_secret_access_key)
 
     def _build_tag_filter(self, tags):
         """ Convert a dict in to a tag filter.
@@ -29,12 +29,18 @@ class EC2TagManager:
         for k, v in tags.iteritems():
             tag_filter['tag:%s' % k] = v
         return tag_filter
-            
 
-    def get_instances(self, **kwargs):
-        """ Return instances that match the given tags. Tags are specified
-            as kwargs, e.g. "get_instances(role='test')
+    def get_instances(self, instance_attr='public_dns_name', **kwargs):
+        """ Return instances that match the given tags.
+
+            Keyword arguments:
+            instance_attr -- attribute of instance(s) to return (default public_dns_name)
+
+            Additional arguments are used to generate tag filter e.g. "get_instances(role='test')
         """
+        if not instance_attr:
+            raise ValueError('instance_attr cannot be None or empty' % instance_attr)
+
         tags = self.common_tags
         for k, v in kwargs.copy().iteritems():
             tags[k] = v
@@ -47,7 +53,10 @@ class EC2TagManager:
             reservations = self.conn[region].get_all_instances(None, tag_filter)
             for res in reservations:
                 for instance in res.instances:
-                    if instance.public_dns_name != '':
+                    instance_value = getattr(instance, instance_attr)
+                    if instance_value:
                         # Terminated/stopped instances will not have a public_dns_name
-                        hosts.append(instance.public_dns_name)
+                        hosts.append(instance_value)
+                    else:
+                        raise ValueError('%s is not an attribute of instance' % instance_attr)
         return hosts
